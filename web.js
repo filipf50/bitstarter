4bb63bf70ac371b8f03ce8411b9e37b3957f4baf
Ubuntu <ubuntu@ip-172-31-20-93.us-west-2.compute.internal>
2013-07-10 23:54:08 +0000

    Modified web.js to print index.html content

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
-
+var fs = require('fs');
+var filename='index.html';
+var message='';
 var app = express.createServer(express.logger());
 
+
+fs.readFile(filename,'utf8',function(err,data){
+    message=data;
+});
+
 app.get('/', function(request, response) {
- response.send('Hellow World 2!');
+ //response.send('Hellow World 2!');
+    response.send(message);
 });
 
 var port = process.env.PORT || 5000;
